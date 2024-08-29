9b3b3f6656e92edfd01b3edc17209769406396b9
Thyérry <thyerry.nunes@gmail.com>
2024-08-29 11:29:36 -0300

    minor tweaks

--- app/src/main/java/com/feirapp/feirapp/network/RetrofitClient.kt
+++ app/src/main/java/com/feirapp/feirapp/network/RetrofitClient.kt
@@ -4,12 +4,11 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val INVOICE_IMPORT_API_URL = "https://701f-45-187-171-214.ngrok-free.app"
-    private const val FEIRAPP_BACKEND_API_URL = "123456789"
+    private const val FEIRAPP_BACKEND_API_URL = "backend-url"
 
     fun create(): GroceryItemApiService {
         val retrofit = Retrofit.Builder()
-            .baseUrl(INVOICE_IMPORT_API_URL)
+            .baseUrl(FEIRAPP_BACKEND_API_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
 
